f320d024ec427283efd36d36a2dc2c2df7292e38
Senfi <senfi.kill@gmail.com>
2025-10-16 17:20:23 +0300

    Add files via upload

---
+++ application/primary/src.py
@@ -0,0 +1,145 @@
+carss = (
+    {
+    "car_name_1": "Жигуль",
+    "car_cost_1": 50000,
+
+    "car_name_2": "Lada Granta Cross",
+    "car_cost_2": 300000,
+
+    "car_name_3": "Audi RS 6",
+    "car_cost_3": 20000000,
+
+    "car_name_4": "BMW M3",
+    "car_cost_4": 10000000,
+
+    "car_name_5": "Mustang GT 500",
+    "car_cost_5": 500000000,
+
+    "car_name_6": "Acura NSX",
+    "car_cost_6": 650000000,
+
+    "car_name_7": "Porsche 911",
+    "car_cost_7": 300000000,
+
+    "car_name_8": "Bugatti Chiron",
+    "car_cost_8": 600000000,
+
+    "car_name_9": "Lamborghini Huracan",
+    "car_cost_9": 950000000
+}
+)
+
+homess = (
+    {
+    "home_name_1": "Коробка",
+    "home_cost_1": 10000,
+
+    "home_name_2": "Сарай",
+    "home_cost_2": 70000,
+
+    "home_name_3": "Гараж",
+    "home_cost_3": 100000,
+
+    "home_name_4": "Комната в общаге",
+    "home_cost_4": 500000,
+
+    "home_name_5": "Однокомнатная квартира",
+    "home_cost_5": 900000,
+
+    "home_name_6": "Коттедж",
+    "home_cost_6": 10000000,
+
+    "home_name_7": "Дом на рублёвке",
+    "home_cost_7": 300000000,
+
+    "home_name_8": "Moscow City",
+    "home_cost_8": 700000000,
+
+    "home_name_9": "Мальдивы",
+    "home_cost_9": 900000000,
+
+    "home_name_10": "Своя планета",
+    "home_cost_10": 1000000000
+}
+)
+
+phoness = (
+    {
+    "phone_name_1": "Тапок",
+    "phone_cost_1": 1000,
+
+    "phone_name_2": "Nokia 3310",
+    "phone_cost_2": 5000,
+
+    "phone_name_3": "Samsung Galaxy S8",
+    "phone_cost_3": 20000,
+
+    "phone_name_4": "Xiaomi Mi Mix",
+    "phone_cost_4": 30000,
+
+    "phone_name_5": "iPhone X",
+    "phone_cost_5": 70000,
+
+    "phone_name_6": "Samsung Z Flip",
+    "phone_cost_6": 100000,
+
+    "phone_name_7": "iPhone 14",
+    "phone_cost_7": 600000,
+
+    "phone_name_8": "iPhone 14 Pro Max",
+    "phone_cost_8": 900000
+}
+)
+
+workss = (
+    {
+    'work_name_1': "Дворник",
+    "work_zp_1": 6000,
+    'work_lvl_1': 1,
+
+    "work_name_2": "Официант",
+    "work_zp_2": 30000,
+    "work_lvl_2": 10,
+
+    "work_name_3": "Повар",
+    "work_zp_3": 70000,
+    "work_lvl_3": 15,
+
+    "work_name_4": "Врач",
+    "work_zp_4": 100000,
+    "work_lvl_4": 25,
+
+    "work_name_5": "Стоматолог",
+    "work_zp_5": 700000,
+    "work_lvl_5": 50,
+
+    "work_name_6": "Инженер",
+    "work_zp_6": 3000000,
+    "work_lvl_6": 70,
+
+    "work_name_7": "Космонавт",
+    "work_zp_7": 8000000,
+    "work_lvl_7": 80,
+
+    "work_name_8": "Программист",
+    "work_zp_8": 10000000,
+    "work_lvl_8": 100
+}
+)
+
+# макс символы
+PREFIX_MAX = 10
+NICKNAME_MAX = 15
+ENERGY_START = 10
+ENERGY_MAX = 50
+
+# эмодзи
+EMOJI_HI = '👋'
+EMOJI_LIKE = '👍'
+EMOJI_DISLIKE = '👎'
+EMOJI_YES = '✅'
+EMOJI_NO = '❌'
+EMOJI_W = '❓'
+EMOJI_WARN = '⚠️'
+SMILE = ['😀', '😃', '😄', '😁', '😆', '😎', '😱']
+EMOJI_SAD = ['😞', '😔', '☹️', '😖', '😢']
